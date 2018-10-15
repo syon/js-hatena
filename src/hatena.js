381d30fe031c839196d3d6f093a96e41f998b6a5
syon <syon.xiii@gmail.com>
2018-10-15 22:56:05 +0900

    :sparkles: Star #get

--- src/hatena.js
+++ src/hatena.js
@@ -1,8 +1,17 @@
 const $ = require("jquery");
 
+const B = {}
+if (location.protocol === 'https:') {
+  B.apiOrigin = 'https://b.hatena.ne.jp'
+  B.starOrigin = 'https://s.hatena.com'
+} else {
+  B.apiOrigin = 'http://api.b.st-hatena.com'
+  B.starOrigin = 'http://s.hatena.com'
+}
+
 class Bookmark {
   static async getEntryCount(pageUrl) {
-    console.log("hatena.js ---- #getEntryCount")
+    console.log("hatena.js ---- #getEntryCount");
     var apiUrl = `http://api.b.st-hatena.com/entry.count?url=${pageUrl}`;
 
     // $.ajax returns Promise.
@@ -13,10 +22,20 @@ class Bookmark {
   }
 
   static async getEntryTotalCount(pageUrl) {
-    return new Error("Not supported on AJAX. (Request Method: OPTIONS is 405 Method Not Allowed)")
+    return new Error("Not works because of 'CORB'.");
+  }
+}
+
+class Star {
+  static async get(pageUrl) {
+    return $.ajax({
+      dataType: "jsonp", // Needs on development
+      url: `${B.apiOrigin}/entry/jsonlite/?url=${encodeURIComponent(pageUrl)}`,
+    });
   }
 }
 
 module.exports = {
-  Bookmark
+  Bookmark,
+  Star
 };
