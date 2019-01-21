d21c5bdfd76b2c1d48474c40470c41ab3dd7a5a0
syon <syon.xiii@gmail.com>
2019-01-21 13:38:50 +0900

    :package: js-hatena

--- App.vue
+++ App.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>Hello!</h1>
+  <main>
+    <h1>js-hatena</h1>
+
+    <h3>Bookmark.getEntryLite</h3>
+    <button @click="getEntryLite">getEntryLite</button>
+    <pre>{{ lite }}</pre>
+  </main>
 </template>
+
+<script>
+import Hatena from "js-hatena";
+
+export default {
+  data() {
+    return {
+      lite: ""
+    }
+  },
+  methods: {
+    async getEntryLite() {
+      const r = await Hatena.Bookmark.getEntryLite("https://example.com");
+      this.lite = JSON.stringify(r, null, 2);
+    }
+  }
+};
+</script>
