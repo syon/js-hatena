febea14ee025c55006b4f9e54860539795698cb9
syon <syon.xiii@gmail.com>
2019-01-21 15:04:20 +0900

    :wrench: vue serve

--- App.vue
+++ App.vue
@@ -4,6 +4,11 @@
 
     <h3>Bookmark.getEntryLite</h3>
     <button @click="getEntryLite">getEntryLite</button>
+
+    <h3>Star.getEntry</h3>
+    <button @click="getEntry">getEntry</button>
+
+    <hr />
     <pre>{{ lite }}</pre>
   </main>
 </template>
@@ -15,11 +20,17 @@ export default {
   data() {
     return {
       lite: ""
-    }
+    };
   },
   methods: {
     async getEntryLite() {
-      const r = await Hatena.Bookmark.getEntryLite("https://example.com");
+      const r = await Hatena.Bookmark.getEntryLite("http://example.com");
+      this.lite = JSON.stringify(r, null, 2);
+    },
+    async getEntry() {
+      // http://b.hatena.ne.jp/syonx/20190121#bookmark-36350794
+      const arg = { user: "syonx", yyyymmdd: "20190121", eid: 36350794 };
+      const r = await Hatena.Star.getEntry(arg);
       this.lite = JSON.stringify(r, null, 2);
     }
   }
