4966c9fc7df15d798fcd62103fa517e414bf087f
syon <syon.xiii@gmail.com>
2019-01-21 16:06:05 +0900

    :sparkles: RKS

--- App.vue
+++ App.vue
@@ -2,6 +2,14 @@
   <main>
     <h1>js-hatena</h1>
 
+    <input type="text" v-model="rawUrl" style="width:100%;">
+
+    <hr>
+
+    <h3>User.getRKS</h3>
+    <button @click="getRKS">getRKS</button>
+    <button @click="addStar">addStar</button>
+
     <h3>Bookmark.getEntryLite</h3>
     <button @click="getEntryLite">getEntryLite</button>
 
@@ -19,10 +27,20 @@ import Hatena from "js-hatena";
 export default {
   data() {
     return {
+      rawUrl: "http://b.hatena.ne.jp/syonx/20190121#bookmark-36350794",
       lite: ""
     };
   },
   methods: {
+    async getRKS() {
+      const r = await Hatena.Star.getRKS(this.rawUrl);
+      this.lite = JSON.stringify(r, null, 2);
+    },
+    async addStar() {
+      const rks = await Hatena.Star.getRKS(this.rawUrl);
+      const r = await Hatena.Star.addStar(this.rawUrl, rks);
+      this.lite = JSON.stringify(r, null, 2);
+    },
     async getEntryLite() {
       const r = await Hatena.Bookmark.getEntryLite("http://example.com");
       this.lite = JSON.stringify(r, null, 2);
