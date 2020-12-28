dda80c6d47af7c5cc12bc98504f3e968e6857f26
syon <syon.xiii@gmail.com>
2020-12-28 14:25:55 +0900

    :zap: hatena.now.sh

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require("./src/hatena");
+module.exports = require('./src/hatena')
--- src/hatena.js
+++ src/hatena.js
@@ -1,170 +1,25 @@
-const fetchJsonp = require("fetch-jsonp");
-const { parseURL } = require("whatwg-url");
+const fetchJsonp = require('fetch-jsonp')
+const axios = require('axios')
 
 const isServer = !process.client
-const isDev = !isServer && window.location.protocol === "http:"
+const isDev = !isServer && window.location.protocol === 'http:'
 
 // https://bookmark.hatenastaff.com/entry/2019/08/26/111011
-const B = {};
+const B = {}
 if (isServer || !isDev) {
-  B.countOrigin = "https://bookmark.hatenaapis.com";
-  B.entryOrigin = "https://b.hatena.ne.jp";
-  B.profileOrigin = "https://profile.hatena.ne.jp";
-  B.starOrigin = "https://s.hatena.com";
-  B.starAddOrigin = "https://s.hatena.ne.jp";
-  B.starImageOrigin = "https://s.st-hatena.com";
+  B.countOrigin = 'https://hatena.now.sh/api'
+  B.entryOrigin = 'https://hatena.now.sh/api'
+  B.profileOrigin = 'https://profile.hatena.ne.jp'
+  B.starOrigin = 'https://s.hatena.com'
+  B.starAddOrigin = 'https://s.hatena.ne.jp'
+  B.starImageOrigin = 'https://s.st-hatena.com'
 } else {
-  B.countOrigin = "https://bookmark.hatenaapis.com";
-  B.entryOrigin = "https://b.hatena.ne.jp";
-  B.profileOrigin = "http://profile.hatena.ne.jp";
-  B.starOrigin = "http://s.hatena.com";
-  B.starAddOrigin = "http://s.hatena.ne.jp";
-  B.starImageOrigin = "http://s.st-hatena.com";
-}
-
-class User {
-  static getProfileImageURL(user) {
-    const apiUrl = `https://cdn.profile-image.st-hatena.com/users/${user}/profile.png`;
-    return apiUrl;
-  }
-
-  static getFavorites(user) {
-    const apiUrl = `${B.profileOrigin}/${user}/favorites.json`;
-    return fetchJsonp(apiUrl, { timeout: 30000 }).then(r => {
-      if (r.ok) return r.json();
-      throw new Error(r);
-    });
-  }
-}
-
-class Bookmark {
-  static getUserPageURL(user) {
-    const apiUrl = `https://b.hatena.ne.jp/${user}/`;
-    return apiUrl;
-  }
-
-  static async getEntryCount(rawPageUrl) {
-    const apiUrl = `${B.countOrigin}/count/entry?url=${rawPageUrl}`;
-    return fetchJsonp(apiUrl, { timeout: 30000 }).then(r => {
-      if (r.ok) return r.json();
-      throw new Error(r);
-    });
-  }
-
-  static async getEntryLite(rawPageUrl) {
-    const url = encodeURIComponent(rawPageUrl);
-    const apiUrl = `${B.entryOrigin}/entry/jsonlite/?url=${url}`;
-    const result = await fetchJsonp(apiUrl, { timeout: 30000 }).then(r => {
-      if (r.ok) return r.json();
-      throw new Error(r);
-    });
-    return result || {};
-  }
-
-  static async getEntryTotalCount() {
-    throw new Error("Not works because of 'CORB'.");
-  }
-}
-
-class Star {
-  static async getEntry({ user, yyyymmdd, eid }) {
-    const uri = `https://b.hatena.ne.jp/${user}/${yyyymmdd}%23bookmark-${eid}`;
-    const encodedUri = Util.encodeURI(uri)
-    const apiUrl = `${B.starOrigin}/entry.json?uri=${encodedUri}`;
-    return fetchJsonp(apiUrl, { timeout: 30000 }).then(r => {
-      if (r.ok) return r.json();
-      throw new Error(r);
-    });
-  }
-
-  static getTotalCount({ uri }) {
-    const encodedUri = Util.encodeURI(uri)
-    const apiUrl = `${B.starAddOrigin}/blog.json?uri=${encodedUri}`;
-    return fetchJsonp(apiUrl, { timeout: 30000 }).then(r => {
-      if (r.ok) return r.json();
-      throw new Error(r);
-    });
-  }
-
-  static getEntryCountImageURL({ user, yyyymmdd, eid }) {
-    const uri = `https://b.hatena.ne.jp/${user}/${yyyymmdd}%23bookmark-${eid}`;
-    const encodedUri = Util.encodeURI(uri)
-    const apiUrl = `${B.starImageOrigin}/entry.count.image?uri=${encodedUri}`;
-    return apiUrl;
-  }
-
-  static async getArrangedStarSetByEntry(entry) {
-    if (!entry || !entry.bookmarks) return {};
-    const commentedOnly = entry.bookmarks.filter(x => x.comment);
-    const starEntries = await Promise.all(
-      commentedOnly.map(async b => {
-        const se = await Star.getStarEntry(entry.eid, b);
-        se.user = b.user;
-        return se;
-      })
-    );
-    return Star.makeStarSet(starEntries);
-  }
-
-  static makeStarSet(starEntries) {
-    const starSet = {};
-    starEntries.forEach(x => {
-      starSet[x.user] = Star.makeStars(x.entries[0]);
-    });
-    return starSet;
-  }
-
-  static makeStars(entry) {
-    const stars = {};
-    if (entry) {
-      // merge stars by same user (yellow only)
-      const yellow = Array.from(new Set(entry.stars.map(x => x.name))).length;
-      if (yellow > 0) {
-        stars.yellow = yellow;
-      }
-      if (entry.colored_stars) {
-        entry.colored_stars.forEach(cs => {
-          stars[cs.color] = cs.stars.length;
-        });
-      }
-    }
-    return stars;
-  }
-
-  static async getStarEntry(eid, bookmark) {
-    const ymd = bookmark.timestamp.match(/^(20..\/..\/..)/)[1];
-    const yyyymmdd = ymd.replace(/\//g, "");
-    const star = await Star.getEntry({
-      user: bookmark.user,
-      yyyymmdd,
-      eid
-    });
-    return star;
-  }
-
-  static async getEntries(rawPageUrl) {
-    const encodedUri = Util.encodeURI(rawPageUrl);
-    const apiUrl = `${B.starAddOrigin}/entries.json?uri=${encodedUri}`;
-    return fetchJsonp(apiUrl, { timeout: 30000 }).then(r => {
-      if (r.ok) return r.json();
-      throw new Error(r);
-    });
-  }
-
-  static async getRKS(rawPageUrl) {
-    const res = await Star.getEntries(rawPageUrl);
-    return res.rks;
-  }
-
-  static async addStar(rawPageUrl) {
-    const encodedUri = Util.encodeURI(rawPageUrl);
-    const rks = await Star.getRKS(rawPageUrl);
-    const apiUrl = `${B.starAddOrigin}/star.add.json?uri=${encodedUri}&rks=${rks}`;
-    return fetchJsonp(apiUrl, { timeout: 30000 }).then(r => {
-      if (r.ok) return r.json();
-      throw new Error(r);
-    });
-  }
+  B.countOrigin = 'https://hatena.now.sh/api'
+  B.entryOrigin = 'https://hatena.now.sh/api'
+  B.profileOrigin = 'http://profile.hatena.ne.jp'
+  B.starOrigin = 'http://s.hatena.com'
+  B.starAddOrigin = 'http://s.hatena.ne.jp'
+  B.starImageOrigin = 'http://s.st-hatena.com'
 }
 
 class Util {
@@ -175,20 +30,170 @@ class Util {
   }
 
   static isEncoded(uri) {
-    uri = uri || ''
-    return uri !== decodeURIComponent(uri)
+    const u = uri || ''
+    return u !== decodeURIComponent(u)
   }
 
   static fullyDecodeURI(uri) {
+    let u = uri
     while (Util.isEncoded(uri)) {
-      uri = decodeURIComponent(uri)
+      u = decodeURIComponent(uri)
     }
-    return uri
+    return u
+  }
+}
+
+class User {
+  static getProfileImageURL(user) {
+    const apiUrl = `https://cdn.profile-image.st-hatena.com/users/${user}/profile.png`
+    return apiUrl
+  }
+
+  static getFavorites(user) {
+    const apiUrl = `${B.profileOrigin}/${user}/favorites.json`
+    return fetchJsonp(apiUrl, { timeout: 30000 }).then((r) => {
+      if (r.ok) return r.json()
+      throw new Error(r)
+    })
+  }
+}
+
+class Bookmark {
+  static getUserPageURL(user) {
+    const apiUrl = `https://b.hatena.ne.jp/${user}/`
+    return apiUrl
+  }
+
+  static async getEntryCount(rawPageUrl) {
+    const apiUrl = `${B.countOrigin}/bookmark/getEntryCount`
+    const config = {
+      params: {
+        url: rawPageUrl,
+      },
+      timeout: 30000,
+    }
+    return axios(apiUrl, config).then((res) => res.data)
+  }
+
+  static async getEntryLite(rawPageUrl) {
+    const apiUrl = `${B.countOrigin}/bookmark/getEntryLite`
+    const config = {
+      params: {
+        url: encodeURIComponent(rawPageUrl),
+      },
+      timeout: 30000,
+    }
+    return axios(apiUrl, config).then((res) => res.data)
+  }
+
+  static async getEntryTotalCount() {
+    throw new Error("Not works because of 'CORB'.")
+  }
+}
+
+class Star {
+  static async getEntry({ user, yyyymmdd, eid }) {
+    const uri = `https://b.hatena.ne.jp/${user}/${yyyymmdd}%23bookmark-${eid}`
+    const encodedUri = Util.encodeURI(uri)
+    const apiUrl = `${B.starOrigin}/entry.json?uri=${encodedUri}`
+    return fetchJsonp(apiUrl, { timeout: 30000 }).then((r) => {
+      if (r.ok) return r.json()
+      throw new Error(r)
+    })
+  }
+
+  static getTotalCount({ uri }) {
+    const encodedUri = Util.encodeURI(uri)
+    const apiUrl = `${B.starAddOrigin}/blog.json?uri=${encodedUri}`
+    return fetchJsonp(apiUrl, { timeout: 30000 }).then((r) => {
+      if (r.ok) return r.json()
+      throw new Error(r)
+    })
+  }
+
+  static getEntryCountImageURL({ user, yyyymmdd, eid }) {
+    const uri = `https://b.hatena.ne.jp/${user}/${yyyymmdd}%23bookmark-${eid}`
+    const encodedUri = Util.encodeURI(uri)
+    const apiUrl = `${B.starImageOrigin}/entry.count.image?uri=${encodedUri}`
+    return apiUrl
+  }
+
+  static async getArrangedStarSetByEntry(entry) {
+    if (!entry || !entry.bookmarks) return {}
+    const commentedOnly = entry.bookmarks.filter((x) => x.comment)
+    const starEntries = await Promise.all(
+      commentedOnly.map(async (b) => {
+        const se = await Star.getStarEntry(entry.eid, b)
+        se.user = b.user
+        return se
+      })
+    )
+    return Star.makeStarSet(starEntries)
+  }
+
+  static makeStarSet(starEntries) {
+    const starSet = {}
+    starEntries.forEach((x) => {
+      starSet[x.user] = Star.makeStars(x.entries[0])
+    })
+    return starSet
+  }
+
+  static makeStars(entry) {
+    const stars = {}
+    if (entry) {
+      // merge stars by same user (yellow only)
+      const yellow = Array.from(new Set(entry.stars.map((x) => x.name))).length
+      if (yellow > 0) {
+        stars.yellow = yellow
+      }
+      if (entry.colored_stars) {
+        entry.colored_stars.forEach((cs) => {
+          stars[cs.color] = cs.stars.length
+        })
+      }
+    }
+    return stars
+  }
+
+  static async getStarEntry(eid, bookmark) {
+    const ymd = bookmark.timestamp.match(/^(20..\/..\/..)/)[1]
+    const yyyymmdd = ymd.replace(/\//g, '')
+    const star = await Star.getEntry({
+      user: bookmark.user,
+      yyyymmdd,
+      eid,
+    })
+    return star
+  }
+
+  static async getEntries(rawPageUrl) {
+    const encodedUri = Util.encodeURI(rawPageUrl)
+    const apiUrl = `${B.starAddOrigin}/entries.json?uri=${encodedUri}`
+    return fetchJsonp(apiUrl, { timeout: 30000 }).then((r) => {
+      if (r.ok) return r.json()
+      throw new Error(r)
+    })
+  }
+
+  static async getRKS(rawPageUrl) {
+    const res = await Star.getEntries(rawPageUrl)
+    return res.rks
+  }
+
+  static async addStar(rawPageUrl) {
+    const encodedUri = Util.encodeURI(rawPageUrl)
+    const rks = await Star.getRKS(rawPageUrl)
+    const apiUrl = `${B.starAddOrigin}/star.add.json?uri=${encodedUri}&rks=${rks}`
+    return fetchJsonp(apiUrl, { timeout: 30000 }).then((r) => {
+      if (r.ok) return r.json()
+      throw new Error(r)
+    })
   }
 }
 
 module.exports = {
   User,
   Bookmark,
-  Star
-};
+  Star,
+}
