e735613092f6820e3160e0dde4a8cf7dd6a13764
syon <syon.xiii@gmail.com>
2020-12-31 22:55:35 +0900

    :shirt: renew ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,20 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    es6: true,
   },
   parserOptions: {
-    parser: "babel-eslint",
-    ecmaVersion: 2018
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
   },
-  extends: [
-    "airbnb-base",
-    "plugin:prettier/recommended",
-  ],
-  plugins: [
-    "prettier"
-  ],
+  extends: ['prettier', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  }
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 'error',
+    'no-undef': 'error',
+  },
+}
